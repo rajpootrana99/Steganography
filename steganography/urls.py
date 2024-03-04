22d18dcd537056bba898f97b2293370e56bfc09b
Muhammad Rana <muhammadrana523@gmail.com>
2024-03-04 20:31:00 +0500

    working cpanel setup

--- steganography/urls.py
+++ steganography/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("authentication.urls"))
+    path('', include("app.urls"))
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
 urlpatterns += staticfiles_urlpatterns()
