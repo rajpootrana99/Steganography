fd5c09215be659dff588803c6aeb3e95961239c0
Muhammad Bin Zulfiqar <muhammadrao1246@gmail.com>
2024-03-14 15:28:31 +0500

    Audio Video atached with Spread Spectrum

--- app/algorithms/VideoAlgorithm.py
+++ app/algorithms/VideoAlgorithm.py
@@ -7,7 +7,7 @@ import cv2
 import numpy as np
 from PIL import Image
 from moviepy.editor import *
- 
+from steganography.settings import *
 import time, re
 
 # Convert encoding data into 8-bit binary ASCII
@@ -143,7 +143,7 @@ def decode(frame_save_path):
         total_frames = len(glob.glob(f"{frame_save_path}/*.png"))
         for index in range(0, total_frames):
             frame_path = frame_save_path + "\\" + str(index) + ".png"
-            print(frame_path)
+            # print(frame_path)
             # frame_path = frame_path.replace("\\", "/")
             try:
                 decoded_message += decoder(frame_path)
@@ -158,19 +158,18 @@ def decode(frame_save_path):
 
 
 # Example usage:
-# video_path = "mp4.mp4"
-# output_path = "C:/Users/mrao1/Downloads/Steganography Algorithm/Steganography Algorithm/frames"
+print(BASE_DIR)
+video_path = str(BASE_DIR).replace("\\", "/") + "/static/assets/samples/mp4.mp4"
+print(video_path)
+output_path =  str(BASE_DIR).replace("\\", "/") + "/static/assets/samples/frames/"
 # # text_to_hide = "Hello, world!"
-# text_to_hide = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+text_to_hide = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
 
 
-# start_time = time.time()
-# encode(video_path, text_to_hide, output_path)
-# print("--- %s seconds ---" % (time.time() - start_time))
+start_time = time.time()
+encode(video_path, text_to_hide, output_path)
+print("--- %s seconds ---" % (time.time() - start_time))
 
-# start_time = time.time()
-# print("Decoded Message:", decode(output_path))
-# print("--- %s seconds ---" % (time.time() - start_time))
-# decoded_text = decode_text_from_video(output_path)
-# print("Hidden text in the video:")
-# print(decoded_text)
+start_time = time.time()
+print("Decoded Message:", decode(output_path))
+print("--- %s seconds ---" % (time.time() - start_time))
