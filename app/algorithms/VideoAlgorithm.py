99d9e43c0edb243ad47c5c396d925814fa382c75
Muhammad Bin Zulfiqar <muhammadrao1246@gmail.com>
2024-03-14 15:47:39 +0500

    Audio Video atached with Spread Spectrum

--- app/algorithms/VideoAlgorithm.py
+++ app/algorithms/VideoAlgorithm.py
@@ -161,7 +161,7 @@ def decode(frame_save_path):
 print(BASE_DIR)
 video_path = str(BASE_DIR).replace("\\", "/") + "/static/assets/samples/mp4.mp4"
 print(video_path)
-output_path =  str(BASE_DIR).replace("\\", "/") + "/static/assets/samples/frames/"
+output_path =  str(BASE_DIR).replace("\\", "/") + "/static/assets/samples/frames"
 # # text_to_hide = "Hello, world!"
 text_to_hide = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
 
