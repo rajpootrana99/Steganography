4d18589211c0855995d0d5ae7920a0e67a6bcfbd
Muhammad Bin Zulfiqar <muhammadrao1246@gmail.com>
2024-03-25 01:05:22 +0500

    Video Encoding Issue Fixed Validation applied to message length

--- steganography/settings.py
+++ steganography/settings.py
@@ -196,8 +196,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = os.path.join(BASE_DIR, '')
+print(BASE_DIR)
 # cpanel
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
