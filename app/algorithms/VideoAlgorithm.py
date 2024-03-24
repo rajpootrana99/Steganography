412ae07201dd28c4bb48f0b7929fe1148cbc78a0
Muhammad Bin Zulfiqar <muhammadrao1246@gmail.com>
2024-03-25 01:23:30 +0500

    Video Encoding Issue Fixed Validation applied to message length

--- app/algorithms/VideoAlgorithm.py
+++ app/algorithms/VideoAlgorithm.py
@@ -294,21 +294,21 @@ def decode(frame_save_path):
 
 # Example usage:
 # print(BASE_DIR)
-# video_path = str(BASE_DIR).replace("\\", "/") + "/static/assets/samples/500kb.mp4"
-# print(video_path)
-# output_path =  str(BASE_DIR).replace("\\", "/") + "/static/assets/samples/frames"
-# text_to_hide = "Hello, world!"
+video_path = str(BASE_DIR).replace("\\", "/") + "/static/assets/samples/500kb.mp4"
+print(video_path)
+output_path =  str(BASE_DIR).replace("\\", "/") + "/static/assets/samples/frames"
+text_to_hide = "Hello, world!"
 # text_to_hide = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
 
 
-# start_time = time.time()
+start_time = time.time()
 # # print(video_to_frames(video_path, output_path))
-# encode(video_path, text_to_hide, output_path)
-# print("--- %s seconds ---" % (time.time() - start_time))
+encode(video_path, text_to_hide, output_path)
+print("--- %s seconds ---" % (time.time() - start_time))
 
 
 
-# start_time = time.time()
-# print("Decoded Message:", decode(output_path))
-# print("--- %s seconds ---" % (time.time() - start_time))
-# exit(0)
+start_time = time.time()
+print("Decoded Message:", decode(output_path))
+print("--- %s seconds ---" % (time.time() - start_time))
+exit(0)
